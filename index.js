5fe84d7861cde32cb4323d59a2aad7b768cffe06
theague <theague@gmail.com>
2020-08-17 18:02:51 -0700

    first efforts, full task not working

---
+++ index.js
@@ -0,0 +1,29 @@
+let today = new Date();
+
+function dayCheck (x) {
+    let y = Date.parse(x);
+    if (y > today) {
+        return `Error`;
+    } else {
+        return x;
+    }
+}
+
+let result = dayCheck(`08/17/2020`)
+const event = Date.parse(result);
+const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+
+//console.log(event); //returns date in millis and I know it's working
+
+console.log(event.toLocaleDateString('en-US', options));
+
+
+
+/*
+;
+let weekday = today.getDay();
+let options = {weekday: 'long'};
+
+console.log(new Intl.DateTimeFormat('en-US',options).format(today));
+
+*/
