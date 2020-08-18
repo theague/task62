5754378d8802a7809bb0953082e49121f646d092
Jaime Rios <jaime.rios.dev@outlook.com>
2020-08-18 09:13:30 -0500

    Comments

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ function dayCheck (x) {
 }
 
 let result = dayCheck(`08/17/2020`)
-const event = Date.parse(result);
+const newDate = Date.parse(result);
 const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
 
-//console.log(event); //returns date in millis and I know it's working
-
-console.log(event.toLocaleDateString('en-US', options)); //this gives me an error
+console.log(today.toLocaleDateString('en-US', options));
+// Wednesday August 12, 2020
